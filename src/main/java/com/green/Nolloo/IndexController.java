dae79aeb86b01905cb4af58d9305e63d10d911bc
gui4458 <gui4458@gmail.com>
2024-02-23 16:48:08 +0900

    jeon

--- src/main/java/com/green/Nolloo/IndexController.java
+++ src/main/java/com/green/Nolloo/IndexController.java
@@ -1,7 +1,8 @@
 package com.green.Nolloo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String maim(){
