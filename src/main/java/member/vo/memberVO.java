8446d47611228dc25463cc78beb4daadca863966
jadu9942 <jadu9942@gmail.com>
2024-02-23 16:20:37 +0900

    초ê¸memberVO, join insert

---
+++ src/main/java/member/vo/memberVO.java
@@ -0,0 +1,24 @@
+package member.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class memberVO {
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private String memberBirth;
+    private String gender;
+    private String memberEmail;
+    private String memberTel;
+    private String memberAddr;
+    private String addrDetail;
+    private String joinDate;
+    private String memberRoll;
+
+
+}
