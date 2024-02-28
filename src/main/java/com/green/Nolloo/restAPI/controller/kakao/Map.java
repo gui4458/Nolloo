a9e2880dd2a61fe9e41550958e978953c0c5e116
Woosung <youngz21@gmail.com>
2024-02-28 17:42:54 +0900

    add map function

--- src/main/java/com/green/Nolloo/restAPI/controller/kakao/Map.java
+++ src/main/java/com/green/Nolloo/restAPI/controller/kakao/Map.java
@@ -7,6 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Map {
 
+    public double getDistanceBetweenPointsNew(double latitude1, double longitude1, double latitude2, double longitude2, String unit) {
+        double theta = longitude1 - longitude2;
+        double distance = 60 * 1.1515 * (180/Math.PI) * Math.acos(
+                Math.sin(latitude1 * (Math.PI/180)) * Math.sin(latitude2 * (Math.PI/180)) +
+                        Math.cos(latitude1 * (Math.PI/180)) * Math.cos(latitude2 * (Math.PI/180)) * Math.cos(theta * (Math.PI/180))
+        );
+        if (unit.equals("miles")) {
+            return Math.round(distance);
+        } else if (unit.equals("kilometers")) {
+            return Math.round(distance * 1.609344);
+        } else {
+            return 0;
+        }
+    }
+
+
     @GetMapping("/position")
     public String pos() {
 
@@ -17,6 +33,12 @@ public class Map {
     public String findItems() {
 
         return "/content/restAPI/find-items";
+    }
+
+    @GetMapping("/circle")
+    public String circle() {
+
+        return "/content/restAPI/circle";
     }
 
 
