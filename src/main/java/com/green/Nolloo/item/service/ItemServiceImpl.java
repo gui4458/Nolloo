3465e1f7bf3636e595d9beb82a06cf81dbd94666
trueSolju <jusol0701@gmail.com>
2024-02-28 16:27:58 +0900

    name

--- src/main/java/com/green/Nolloo/item/service/ItemServiceImpl.java
+++ src/main/java/com/green/Nolloo/item/service/ItemServiceImpl.java
@@ -17,26 +17,26 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public void insertParty(ItemVO itemVO) {
 
-        sqlSession.insert("boardMapper.insertBoard",itemVO);
+        sqlSession.insert("itemMapper.insertParty",itemVO);
     }
     //item 목록조회
     @Override
     public List<ItemVO> selectPartyList() {
-        return sqlSession.selectList("boardMapper.selectBoardList");
+        return sqlSession.selectList("itemMapper.selectPartyList");
     }
 
     @Override
     public ItemVO selectPartyDetail(ItemVO itemVO) {
-        return sqlSession.selectOne("boardMapper.selectBoardDetail", itemVO);
+        return sqlSession.selectOne("itemMapper.selectPartyDetail", itemVO);
     }
 
     @Override
     public void deleteParty(ItemVO itemVO) {
-        sqlSession.delete("boardMapper.deleteBoard",itemVO);
+        sqlSession.delete("itemMapper.deleteParty",itemVO);
     }
 
     @Override
     public void updateParty(ItemVO itemVO) {
-        sqlSession.update("boardMapper.updateBoard",itemVO);
+        sqlSession.update("itemMapper.updateParty",itemVO);
     }
 }
