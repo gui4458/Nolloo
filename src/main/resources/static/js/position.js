aabbc59267b43c049d43c087f48f468c11007210
Woosung <youngz21@gmail.com>
2024-03-08 11:14:28 +0900

    detail position complete

--- src/main/resources/static/js/position.js
+++ src/main/resources/static/js/position.js
@@ -1,74 +1,40 @@
-var urlParams = new URLSearchParams(window.location.search);
-var ic = urlParams.get('itemCode');
-console.log(ic)
+let lat = parseFloat(itemX.value); 
+let lng = parseFloat(itemY.value); 
 
-getMapData(ic);
+let data = {
+    lat: lat,
+    lng: lng
+};
+console.log(data)
 
+drawMap(data);
 
-function getMapData(ic) {
-    fetch('/itemDetailForm', { //요청경로
-        method: 'GET',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
-        //컨트롤러로 전달할 데이터
-        body: new URLSearchParams({
-           // 데이터명 : 데이터값
-           itemCode : ic
-        })
-    })
-    .then((response) => {
-        if(!response.ok){
-            alert('fetch error!\n컨트롤러로 통신중에 오류가 발생했습니다.');
-            return ;
-        }
-    
-        // return response.text(); //컨트롤러에서 return하는 데이터가 없거나 int, String 일 때 사용
-        return response.json(); //나머지 경우에 사용
-    })
-    //fetch 통신 후 실행 영역
-    .then((data) => {//data -> controller에서 리턴되는 데이터!
-        console.log(data);
-        drawMap(data);
-    })
-    //fetch 통신 실패 시 실행 영역
-    .catch(err=>{
-        alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
-        console.log(err);
-    });
-    
-}
+function drawMap(pos) {
+    console.log(pos.lat);
+    console.log(pos.lng);
 
-
-function drawMap(data) {
-
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-
-    mapOption = { 
-        center: new kakao.maps.LatLng(data.itemX, data.itemY), // 지도의 중심좌표
+    var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
+    var mapOption = { 
+        center: new kakao.maps.LatLng(pos.lat, pos.lng), // 지도의 중심좌표
         level: 4 // 지도의 확대 레벨
     };
     
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
     
-    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'; // 마커이미지의 주소입니다    
+    var imageSize = new kakao.maps.Size(64, 69); // 마커이미지의 크기입니다
+    var imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
     
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(data.itemX, data.itemY); // 마커가 표시될 위치입니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    var markerPosition = new kakao.maps.LatLng(pos.lat, pos.lng); // 마커가 표시될 위치입니다
     
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
-    position: markerPosition, 
-    image: markerImage // 마커이미지 설정 
+        position: markerPosition, 
+        image: markerImage // 마커이미지 설정 
     });
     
     // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);  
-
 }
-
-
