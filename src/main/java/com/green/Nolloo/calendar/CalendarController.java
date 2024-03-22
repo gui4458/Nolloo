d6e01bf3905ddc944069ebe95ba19a9d37394598
gui4458 <gui4458@gmail.com>
2024-03-22 12:17:31 +0900

    dev

--- src/main/java/com/green/Nolloo/calendar/CalendarController.java
+++ src/main/java/com/green/Nolloo/calendar/CalendarController.java
@@ -2,10 +2,8 @@ package com.green.Nolloo.calendar;
 
 import com.green.Nolloo.item.service.ItemService;
 import com.green.Nolloo.item.vo.ItemVO;
-import com.green.Nolloo.search.vo.SearchVO;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +13,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/calendar")
-public class calendarController {
+public class CalendarController {
     @Resource(name="itemService")
     private ItemService itemService;
 
--- src/main/resources/static/js/fragment/user_header.js
+++ src/main/resources/static/js/fragment/user_header.js
@@ -92,4 +92,14 @@ function goCalendar(){
 
 }
 
-    
+function goParty(num){
+  num = parseInt(num)
+  location.href=`/item/list?chkCode=${num}`
+  
+
+  
+}
+function goFestival(num){
+  num = parseInt(num)
+  location.href=`/item/list?chkCode=${num}`
+}
