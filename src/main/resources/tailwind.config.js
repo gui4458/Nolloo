51ec524f4b81d2e6c9e99ac4e3dda6ad8da0d5f1
jadu9942 <jadu9942@gmail.com>
2024-04-05 15:07:17 +0900

    0405

---
+++ src/main/resources/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //content: ["D:/01-STUDY/dev/download/OneDrive/Nolloo/src/main/resources/templates/**/*.{html,js}"],
+  content: ['../templates/**/*.html'],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
