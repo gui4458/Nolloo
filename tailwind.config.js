4013e65bbcf68ab966d25ba87099c8d60b5c2b5c
jadu9942 <jadu9942@gmail.com>
2024-04-05 14:15:04 +0900

    0404

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["D:/01-STUDY/dev/download/OneDrive/Nolloo/src/main/resources/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'aaa': ['Noto Sans KR', 'Arial', 'sans-serif'],
+        'bbb': ["Single Day", 'cursive']
+      }
+    },
   },
   plugins: [],
 }
