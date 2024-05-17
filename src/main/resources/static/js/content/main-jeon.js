71c4712d9673c905f954d0180cc2fee15f295ba8
gui4458 <gui4458@gmail.com>
2024-05-17 14:13:56 +0900

    dev

--- src/main/resources/static/js/content/main-jeon.js
+++ src/main/resources/static/js/content/main-jeon.js
@@ -16,6 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 });
 
+
 // 초기 아이템 가져오는 함수
 function fetchInitialItems() {
     limit = 9;
@@ -54,7 +55,7 @@ function eventScroll() {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         fetchItems();
     }
-};
+}
 
 
 // 스크롤 이벤트 리스너 등록
@@ -104,71 +105,57 @@ function fetchItems() {
 
 // 초기 아이템 표시 함수
 function displayItems(items) {
-    let today = new Date();
-
-    let year = today.getFullYear();
-    let month = ('0' + (today.getMonth() + 1)).slice(-2);
-    let day = ('0' + today.getDate()).slice(-2);
-
-    let dateString = year + '-' + month + '-' + day;
-
     if (offset >= items.dataCnt) {
         itemHtml += '<div class="text-center w-[70%] mb-[70px] border-2 bg-white shadow-lg mx-auto h-[50px] p-3 rounded-lg text-base col-start-1 col-span-3">새로운 글이 없습니다.</div>'
 
         window.removeEventListener('scroll', eventScroll);
     } else {
-
         const itemListContainer = document.getElementById('itemListContainer');
         items.itemList.forEach(item => {
-            console.log(dateString,item.itemStartDate)
-            console.log(dateString > item.itemStartDate)
             let wishchk = items.wishCodeList.includes(item.itemCode)
             itemHtml = itemHtml + `
 
-                    <div class="item-lazy bg-white shadow-xl shadow-slate-900/5 rounded-lg group" onclick="selectItemCode(${item.itemCode}, this)">
+                    <div class="item-lazy bg-white shadow-xl shadow-slate-900/5 rounded-lg group" onclick="selectItemCode(${item.itemCode})">
                     
                         <div class="flex flex-row p-3 lg:flex-col">
-                            <div class="image-container relative w-24 h-24 lg:w-full lg:h-56 bg-cover bg-center rounded-full lg:rounded-lg overflow-hidden">`
-            if (item.cateCode != 2) {
+                        <div class="relative w-24 h-24 lg:w-full lg:h-56 bg-cover bg-center rounded-full lg:rounded-lg overflow-hidde">`
+                        // 그림 안에 하트
+                        // image-container  relative w-24 h-24 lg:w-full lg:h-56 bg-cover bg-center rounded-full lg:rounded-lg overflow-hidden
+                        // <div class="image-container relative w-24 h-24 lg:w-full lg:h-56 bg-cover bg-center rounded-full lg:rounded-lg overflow-hidden">
+            if (item.cateCode == 1) {
 
-                itemHtml += `<img class="object-cover w-full h-full group-hover:scale-110 transition-all duration-[500ms]" src="/upload/itemSolo/${item.imgList[0].attachedFileName}">
-                            `
+                itemHtml += `<img class="object-cover w-full h-full group-hover:scale-110 transition-all duration-[500ms]" src="/upload/itemSolo/${item.imgList[0].attachedFileName}" alt="">
+                                        `
 
             }
             if (item.cateCode == 2) {
 
-                itemHtml += `<img class="object-cover w-full h-full group-hover:scale-110 transition-all duration-[500ms]" src="/upload/item/${item.imgList[0].attachedFileName}">
+                itemHtml += `<img class="object-cover w-full h-full group-hover:scale-110 transition-all duration-[500ms]" src="/upload/item/${item.imgList[0].attachedFileName}" alt="">
                                         `
 
             }
 
-
-            if(dateString > item.itemStartDate && dateString > item.itemEndDate){
-                itemHtml += `<div class="justify-center" style="padding: 90px; font-size : 1.5rem; height:100%; position: absolute; left: 50%; top: 50%; background-color: rgba(165, 165, 165, 0.8); font-weight: bold; width: 100%; text-align: center; transform: translate(-50%,-50%);">종료된 ${item.cateName} 입니다.</div>`
-            }else if(item.cateCode != 2 && item.peopleCnt == item.itemPeople){
-                itemHtml += `<div class="justify-center" style="padding: 100px; font-size : 1.5rem; height:100%; position: absolute; left: 50%; top: 50%; background-color: rgba(165, 165, 165, 0.8); font-weight: bold; width: 100%; text-align: center; transform: translate(-50%,-50%);">인원마감</div>`
-            }
-
-
-
-            // <span id="heart" class="wishDelete-div text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="heart(this,${item.itemCode},${wishchk},${loginId},event)"><i class="ri-heart-3-fill"></i></sapn>
-            itemHtml += `
-            <div>
-                            <span id="" class="wishDelete-div text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="heart(this,${item.itemCode},${wishchk},event)">
-                            `
-            if (loginId != "") {
+            if (loginId != null) {
                 if (wishchk) {
-                    itemHtml += '<i class="ri-heart-3-fill" ></i>'
-                } else {
-                    itemHtml += '<i class="ri-heart-3-line"></i>'
+                    itemHtml += `
+                                            <span class="wishDelete-div text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="wishDelete(this,${item.itemCode})"><i class="ri-heart-3-fill"></i></sapn>
 
+                                                `
+                } else {
+                    itemHtml += `
+                                            <span class="wishAdd-div text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="wishAdd(this,${item.itemCode})"><i class="ri-heart-3-line"></i></sapn>
+                                        
+                                        `
                 }
             } else {
-                itemHtml += '<i class="ri-heart-3-line"></i>'
+                itemHtml += `
+                                                    
+                                            
+                                            <span class="text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="goLogin(event)"><i class="ri-heart-3-line"></i></sapn>
+                                                
+                                                `
             }
             itemHtml += `
-                            </span>                        
-                    </div>
                     </div>
                             <div class="ml-5 lg:ml-0 lg:mt-3">
                                 <figcaption class="font-medium">
@@ -199,112 +186,103 @@ function displayItems(items) {
 
 
 
-
 }
 
 // 하트 추가 및 삭제 함수
 // 하트 누르면 추가
-
-window.heart = heart;
-//하트 누르면 삭제
-function heart(divTag, itemCode, wishchk, e) {
-    let heartDiv = divTag.parentElement;
+function wishAdd(divTag, itemCode, e) {
+    const head = divTag;
     e.stopPropagation();
-    let strHeart = ''
-    if (loginId != "") {
-        if (wishchk) {
-            fetch('/wish/wishDelete', { //요청경로
-                method: 'POST',
-                cache: 'no-cache',
-                headers: {
-                    'Content-Type': 'application/json; charset=UTF-8'
-                },
-                //컨트롤러로 전달할 데이터
-                body: JSON.stringify({
-                    // 데이터명 : 데이터값
-                    itemCode: itemCode
+    fetch('/wish/insertWish', { //요청경로
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        //컨트롤러로 전달할 데이터
+        body: JSON.stringify({
+            // 데이터명 : 데이터값
+            itemCode: itemCode
 
-                })
-            })
-                .then((response) => {
-                    return response.text();
-                    // return response.json(); //나머지 경우에 사용
-                })
-                //fetch 통신 후 실행 영역
-                .then((data) => {//data -> controller에서 리턴되는 데이터!
-                    // const result1 = confirm('관심목록에 상품을 등록했습니다.\n관심목록 페이지로 이동할까요?')
-                    //     if (result1) {
-                    //         location.href = `/wish/goWishList`;
-                    //     }
-                    strHeart = `
-                    <span class="text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="heart(this,${itemCode},${false},event)"><i class="ri-heart-3-line"></i></sapn>
-        
-                `
+        })
+    })
+        .then((response) => {
+            return response.json(); //나머지 경우에 사용
+        })
+        //fetch 통신 후 실행 영역
+        .then((data) => {//data -> controller에서 리턴되는 데이터!
+            // const result1 = confirm('관심목록에 상품을 등록했습니다.\n관심목록 페이지로 이동할까요?')
+            //     if (result1) {
+            //         location.href = `/wish/goWishList`;
+            //     }
 
-                    heartDiv.replaceChildren(heartDiv.textContent = '');
-                    heartDiv.insertAdjacentHTML("afterbegin", strHeart)
+            const strDelete = `
+            <span class="text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="wishDelete(this,${itemCode})"><i class="ri-heart-3-fill"></i></sapn>
+
+        `
+            head.replaceChildren(head.textContent = '');
+            head.insertAdjacentHTML("afterbegin", strDelete)
 
 
+            // location.href = '/item/list'
 
-                })
-                //fetch 통신 실패 시 실행 영역
-                .catch(err => {
-                    alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
-                    console.log(err);
-                });
-        } else {
-            fetch('/wish/insertWish', { //요청경로
-                method: 'POST',
-                cache: 'no-cache',
-                headers: {
-                    'Content-Type': 'application/json; charset=UTF-8'
-                },
-                //컨트롤러로 전달할 데이터
-                body: JSON.stringify({
-                    // 데이터명 : 데이터값
-                    itemCode: itemCode
-
-                })
-            })
-                .then((response) => {
-                    return response.json(); //나머지 경우에 사용
-                })
-                //fetch 통신 후 실행 영역
-                .then((data) => {//data -> controller에서 리턴되는 데이터!
-                    // const result1 = confirm('관심목록에 상품을 등록했습니다.\n관심목록 페이지로 이동할까요?')
-                    //     if (result1) {
-                    //         location.href = `/wish/goWishList`;
-                    //     }
-
-                    strHeart = `
-                    <span class="text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="heart(this,${itemCode},${true},event)"><i class="ri-heart-3-fill"></i></sapn>
-        
-                `
-
-                    heartDiv.replaceChildren(heartDiv.textContent = '');
-                    heartDiv.insertAdjacentHTML("afterbegin", strHeart)
-
-
-
-                    // location.href = '/item/list'
-
-                })
-                //fetch 통신 실패 시 실행 영역
-                .catch(err => {
-                    alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
-                    console.log(err);
-                });
-        }
-
-    } else {
-        alert('로그인을 해주세요.');
-
-    }
-
-
+        })
+        //fetch 통신 실패 시 실행 영역
+        .catch(err => {
+            alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
+            console.log(err);
+        });
 }
 
+//하트 누르면 삭제
+function wishDelete(divTag, itemCode, e) {
+    const head = divTag
 
+    e.stopPropagation();
+
+    fetch('/wish/wishDelete', { //요청경로
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        //컨트롤러로 전달할 데이터
+        body: JSON.stringify({
+            // 데이터명 : 데이터값
+            itemCode: itemCode
+
+        })
+    })
+        .then((response) => {
+            return response.text();
+            // return response.json(); //나머지 경우에 사용
+        })
+        //fetch 통신 후 실행 영역
+        .then((data) => {//data -> controller에서 리턴되는 데이터!
+            // const result1 = confirm('관심목록에 상품을 등록했습니다.\n관심목록 페이지로 이동할까요?')
+            //     if (result1) {
+            //         location.href = `/wish/goWishList`;
+            //     }
+            const strInsert = `
+            <span class="text-red-500 absolute right-[10px] top-[5px] text-[25px] cursor-pointer" onclick="wishAdd(this,${itemCode})"><i class="ri-heart-3-line"></i></sapn>
+
+        `
+            head.replaceChildren(head.textContent = '');
+            head.insertAdjacentHTML("afterbegin", strInsert)
+
+        })
+        //fetch 통신 실패 시 실행 영역
+        .catch(err => {
+            alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
+            console.log(err);
+        });
+}
+
+// 로그인 함수
+function gologin(event) {
+    alert('로그인을 해주세요.');
+    event.stopPropagation();
+}
 
 function goChat(itemCode) {
     // 채팅방 URL 생성
@@ -314,9 +292,10 @@ function goChat(itemCode) {
     const windowFeatures = "left=1500,top=100,width=750,height=750,popup";
     window.open(chatRoomUrl, "_blank", windowFeatures);
 }
-
+window.selectItemCode = selectItemCode;
 // modal Detail에 itemCode 보내주기
-function selectItemCode(itemCode, selectedTag, items) {
+function selectItemCode(itemCode) {
+
     fetch('/item/itemDetailForm', { //요청경로
         method: 'POST',
         cache: 'no-cache',
@@ -346,7 +325,8 @@ function selectItemCode(itemCode, selectedTag, items) {
             let wishchk = data.wishCodeList.includes(data.item.itemCode)
 
 
-
+            const loginId = document.querySelector('#loginId').value;
+            console.log(loginId);
 
             const p_tag = document.querySelector('.p-tag');
 
@@ -359,7 +339,7 @@ function selectItemCode(itemCode, selectedTag, items) {
             modalHtml += `
         <!-------- 메인이미지 ------->
             `;
-            if (data.item.cateCode != 2) {
+            if (data.item.cateCode == 1) {
                 data.item.imgList.forEach(function (img, i) {
                     if (img.isMain == 'Y') {
                         modalHtml += `
@@ -418,23 +398,23 @@ function selectItemCode(itemCode, selectedTag, items) {
                 modalHtml += ` <span onclick="goLogin()">채팅바로가기</span>`;
             }
             modalHtml += `</div>
-                <!-------- 도시/제목 ------------>
+                
         <div class="bg-white p-6 p-tag text-center" >
                 <div class="text-2xl">
                     <span class="text-red-300">[${data.item.itemPlace}]</span> 
                     <span class="font-extralight">${data.item.itemTitle}</span>
                 </div>
-                <!------------- 개최자 ----------->
+                
                 <div class="text-right mb-5">
                     ${data.item.memberId}
                 </div>
-                <!------------ 내용 ------------>
+                
                 <div class="">
                     <p class="px-3 my-3">
                         ${data.item.itemContent}
                     </p>
                 </div>
-                <!-------- 날짜 ------------>
+                
                 <span
                 class="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">
                         개최기간</span>
@@ -470,10 +450,12 @@ function selectItemCode(itemCode, selectedTag, items) {
             p_tag.insertAdjacentHTML('afterbegin', modalHtml);
 
             //상품에 대한 지도 붙이기
+
             if(loginId != ''){
                 document.querySelector('.reserve-btn').setAttribute('onclick', `reserveInsert(${data.item.itemCode},${data.reserveCnt},${data.item.cateCode})`);
-            } 
-
+            } //                                  속성 값 추가(이미 있는 속성이면 덮어쓰기 없으면 속성 및 속성 값 추가)
+                
+                
 
             modalToggle();
             setTimeout(() => {
@@ -498,9 +480,13 @@ function modalToggle() {
     document.querySelector('#detail_modal').classList.toggle('flex');
 }
 
+function goLogin(e) {
+    alert('로그인 후 이용해주세요.')
+    e.stopPropagation();
+}
 
-function reserveInsert(itemCode, reserveCnt) {
-
+function reserveInsert(itemCode, reserveCnt, cateCode) {
+    
     if (reserveCnt == 0) {
         fetch('/reserve/partyReserve', { //요청경로
             method: 'POST',
@@ -511,7 +497,8 @@ function reserveInsert(itemCode, reserveCnt) {
             //컨트롤러로 전달할 데이터
             body: JSON.stringify({
                 // 데이터명 : 데이터값
-                itemCode: itemCode
+                'itemCode' : itemCode,
+                'cateCode' : cateCode
             })
         })
             .then((response) => {
@@ -550,24 +537,24 @@ function drawMap(posX, posY) {
     //34.86687999 ITEM_X
     //128.7260471 ITEM_Y
 
-    let mapContainer = document.getElementById('detail_map'); // 지도를 표시할 div 
-    let mapOption = {
+    var mapContainer = document.getElementById('detail_map'); // 지도를 표시할 div 
+    var mapOption = {
         center: new kakao.maps.LatLng(posX, posY), // 지도의 중심좌표
         level: 4 // 지도의 확대 레벨
     };
 
-    let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    let imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'; // 마커이미지의 주소입니다    
-    let imageSize = new kakao.maps.Size(64, 69); // 마커이미지의 크기입니다
-    let imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'; // 마커이미지의 주소입니다    
+    var imageSize = new kakao.maps.Size(64, 69); // 마커이미지의 크기입니다
+    var imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-    let markerPosition = new kakao.maps.LatLng(posX, posY); // 마커가 표시될 위치입니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    var markerPosition = new kakao.maps.LatLng(posX, posY); // 마커가 표시될 위치입니다
 
     // 마커를 생성합니다
-    let marker = new kakao.maps.Marker({
+    var marker = new kakao.maps.Marker({
         position: markerPosition,
         image: markerImage // 마커이미지 설정 
     });
@@ -576,5 +563,8 @@ function drawMap(posX, posY) {
     marker.setMap(map);
     map.relayout();
 
+
 }
+
+
 
